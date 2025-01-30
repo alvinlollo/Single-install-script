95c223cef6bbeba3f8891114b519ed413e7d56cb
alvinlollo <alvin.vilaythong@education.nsw.gov.au>
2025-01-30 06:41:01 +0000

    Asthetics and seperators included between each command

--- install.sh
+++ install.sh
@@ -21,10 +21,13 @@ set -eux pipefail
 # Install prequisites
 sudo apt update
 sudo apt install -y git-core zsh curl git build-essential
-
 # Do not print commands
 set +x
+echo '
 
+    --------------- Docker Install  ---------------
+
+'
 while true; do
     read -p "Do you wish to install Docker? [Y/N]: " yn
     case $yn in
@@ -34,6 +37,11 @@ while true; do
     esac
 done
 
+echo '
+
+    --------------- Homebrew Install  ---------------
+
+'
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -51,7 +59,11 @@ brew install eza fzf gcc thefuck gh
 
 # Disable exit on error 
 set +eux
+echo '
 
+    --------------- Oh-My-zsh Install  ---------------
+
+'
 # Install Oh-My-zsh if not installed
 #   This will install all the plugins used in our .zshrc file
 if [ ! -f ~/.zshrc ]; then
@@ -75,8 +87,14 @@ if [ ! -f ~/.zshrc ]; then
 fi
 
 # Enable exit on error
-set -eux
+set -eu
+set +x
+echo '
 
+    --------------- Oh-My-zsh plugins  ---------------
+
+'
+set -x
 # Backup old config file if it exists
 if [ -f ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc.backup
@@ -89,6 +107,7 @@ curl -fsSL https://raw.githubusercontent.com/alvinlollo/Single-install-script/re
 mkdir -p ~/.fzf/shell
 touch ~/.fzf/shell/key-bindings.zsh
 
+
 # Source .zshrc three times only if fzf has not been initialized
 if [ ! -f ~/.fzf/shell/key-bindings.zsh ]; then
     for i in {1..3}; do
@@ -98,24 +117,53 @@ fi
 
 # Do not print commands
 set +x
+echo '
 
+    --------------- code-server Install  ---------------
+
+'
 # Install code server
 curl -fsSL https://code-server.dev/install.sh | sh
 
+set +x
+echo '
+
+    --------------- GEF Install  ---------------
+
+'
 # Install GEF
 bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
 
 # Do not print commands
-set -x
+set +x
+echo '
 
+    --------------- apt packages Install  ---------------
+
+'
 # Install personal apt packages
 sudo apt install -y git htop golang hugo figlet irssi cmatrix neofetch cowsay fortune-mod tint tty-clock lolcat hugo libsass1 dpkg npm python3 docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin flatpak gnome-software-plugin-flatpak libcurl4-gnutls-dev bsd-mailx needrestart powermgmt-base accountsservice lynx wget curl evince zsh net-tools --fix-missing
+
+# Print commands
+set -x
 sudo apt full-upgrade -y
+
+set +x
+echo '
+
+    --------------- apt packages Install  ---------------
+
+'
+# Print commands
+set -x
 
 # Add an update-all script
 sudo rm -f /usr/local/bin/update-all
 sudo curl -fsSL https://raw.githubusercontent.com/alvinlollo/Single-install-script/refs/heads/main/extras/update-all%20script -o /usr/local/bin/update-all
 sudo chmod +x /usr/local/bin/update-all
+
+# Do not print commands
+set +x
 
 # Done message :)
 echo "Done installing please star my repo: https://github.com/alvinlollo/Single-install-script"
