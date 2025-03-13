049bfdfa8162a43279d6a784ea89a75fd37e22ad
alvinlollo <143326475+alvinlollo@users.noreply.github.com>
2025-03-13 21:51:25 +1100

    Replaced old package names
    Updated pip package name to python-pip and removed python3-full

--- install.sh
+++ install.sh
@@ -114,7 +114,7 @@ echo '
 '
 set -x
 # Install personal apt packages
-sudo apt install -y python3 python3-full pip git htop golang figlet irssi cmatrix neofetch cowsay fortune-mod tint smartmontools udevil samba cifs-utils mergerfs tty-clock lolcat libsass1 dpkg npm python3 needrestart lynx wget curl zsh net-tools network-manager --fix-missing
+sudo apt install -y python3 python-pip git htop golang figlet irssi cmatrix neofetch cowsay fortune-mod tint smartmontools udevil samba cifs-utils mergerfs tty-clock lolcat libsass1 dpkg npm python3 needrestart lynx wget curl zsh net-tools network-manager --fix-missing
 
 # Print commands
 set -x
