fa286ca4793405f12c5dc6f2f976780adb586945
alvinlollo <143326475+alvinlollo@users.noreply.github.com>
2025-06-13 23:43:22 +1000

    Seperaterd zsh install to zsh.sh
    More maintainable if seperate

--- install.sh
+++ install.sh
@@ -18,46 +18,10 @@ set -eux pipefail
 # Install prerequisites
 sudo pacman -Syu git zsh curl git build-essential whiptail uidmap --no-confirm
 
-# Do not print commands
-set +x
-echo '
+# Do not exit on fail
+set +eux
 
-    --------------- Oh-My-zsh Install  ---------------
-
-'
-# Print commands
-set -x
-
-sudo pacman -S zsh fzf --no-confirm
-
-# Installs Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install Oh-My-Zsh plugins
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/z-shell/zsh-eza ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-eza
-git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# Enable exit on error
-set -eu
-set +x
-echo '
-
-    --------------- Oh-My-zsh plugins  ---------------
-
-'
-set -x
-# Backup old config file if it exists
-cp .zshrc .zshrc.backup
-
-# Download and replace config file
-curl -fsSL https://raw.githubusercontent.com/alvinlollo/Single-install-script/refs/heads/main/configs/.zshrc -o ~/.zshrc
-
-# Setup fzf
-mkdir -p ~/.fzf/shell
-touch ~/.fzf/shell/key-bindings.zsh
+curl -fsSL https://raw.githubusercontent.com/alvinlollo/Single-install-script/refs/heads/main/zsh.sh | bash
 
 # Do not print commands
 set +x
