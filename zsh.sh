d7b062dc7b40fba301838824796f0936cc20be4f
alvinlollo <143326475+alvinlollo@users.noreply.github.com>
2025-06-13 23:34:26 +1000

    Ready to integrate into main script as separate script

--- zsh.sh
+++ zsh.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-clear
 echo '
      ____                _       _       _       _ _
     | __ ) _   _    __ _| |_   _(_)_ __ | | ___ | | | ___
@@ -9,28 +8,28 @@ echo '
     |____/ \__  |  \__ _|_| \_/ |_|_| |_|_|\___/|_|_|\___/
             |___/
 
-    --------------- Single Download script ---------------
+    --------------- ZSH Install Script ---------------
 '
 
 # Install prerequisites
+sudo pacman -S zsh fzf --no-confirm
 
+# Do not print commands
+set +x
 echo '
 
     --------------- Oh-My-zsh Install  ---------------
-    
-    Make sure to install zsh and fzf
 
 '
 # Print commands
 set -x
 
+sudo pacman -S zsh fzf --no-confirm
+
 # Installs Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Fail on any command.
-set -eux pipefail
-
-# Install plugins
+# Install Oh-My-Zsh plugins
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/z-shell/zsh-eza ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-eza
