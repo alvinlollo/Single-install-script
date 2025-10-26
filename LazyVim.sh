c1485a8755d44ed594afe48abf548308aa2b593c
alvinlollo <143326475+alvinlollo@users.noreply.github.com>
2025-10-26 23:17:09 +1100

    Create LazyVim.sh

---
+++ LazyVim.sh
@@ -0,0 +1,9 @@
+# LazyVim Install
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+cp ~/.local/share/nvim{,.bak} -r
+cp ~/.local/state/nvim{,.bak} -r
+cp ~/.cache/nvim{,.bak} -r
+
+git clone https://github.com/alvinlollo/LazyVim ~/.config/nvim
