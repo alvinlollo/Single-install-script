16c816c1d4bf60f4e2b6d79182c7bbd734f571dc
alvinlollo <143326475+alvinlollo@users.noreply.github.com>
2025-10-28 22:58:15 +1100

    Add prerequisites installation for SecureBoot script

---
+++ SecureBoot.sh
@@ -0,0 +1,15 @@
+# Install Prerequisites
+sudo pacman -S --needed -noconfirm efibootmgr sbsigntools mokutil sbsigntools mokutil sbctl
+
+sudo sbctl create-keys
+sudo sbctl enroll-keys --microsoft
+sudo sbctl verify
+# use sudo sbctl sign -s for every unsigned item
+sudo sbctl sign -s /boot/EFI/BOOT/BOOTX64.EFI
+sudo sbctl sign -s /boot/grub/x84_64-efi/core.efi
+sudo sbctl sign -s /boot/grub/x86_64-efi/grub.efi
+sudo sbctl sign -s /boot/vmlinuz-linux
+
+sudo sbctl status
+sudo sbctl verify
+echo "Use sudo sbctl sign -s for every unsigned item"
