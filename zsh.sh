e85fc40e220dbf6683f46227afe540d2e8133a7f
alvinlollo <143326475+alvinlollo@users.noreply.github.com>
2025-11-08 23:21:16 +1100

    Add zsh to /etc/shells for shell change

--- zsh.sh
+++ zsh.sh
@@ -68,4 +68,5 @@ curl -fsSL https://raw.githubusercontent.com/alvinlollo/Single-install-script/re
 mkdir -p ~/.fzf/shell
 touch ~/.fzf/shell/key-bindings.zsh
 
+sudo sh -c "echo $(which zsh) >> /etc/shells"
 chsh -s $(which zsh)
