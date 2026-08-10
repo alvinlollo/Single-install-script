743cb24073d9d5467588ee959781e78014dd5740
alvinlollo <alvin.vilaythong@gmail.com>
2026-08-10 21:21:54 +1000

    Remove unused files

--- configs/gdoc.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "Aadv1k/gdoc.vim",
-    lazy = false, -- Set to false to ensure the plugin is loaded immediately on startup
-    cmd = "Gdoc", -- This is the command to start the Google Docs interface
-  },
-  -- ... other plugins ...
-}
